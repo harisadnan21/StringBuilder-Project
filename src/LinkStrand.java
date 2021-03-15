301ba48003b3a0c03ad5979201090005fc057d84
Haris Adnan <haris.adnan@duke.edu>
2021-03-14 20:03:09 -0400

    commit

---
+++ src/LinkStrand.java
@@ -0,0 +1,159 @@
+public class LinkStrand implements IDnaStrand{
+    public LinkStrand(String source) {
+        initialize(source);
+
+    }
+    public LinkStrand(){
+        this("");
+
+    }
+
+
+    private class Node{
+        String info;
+        Node next;
+        public Node(String s){
+            info = s;
+            next = null;
+
+        }
+
+    }
+    private Node myFirst, myLast;
+    private long mySize;
+    private int myAppends;
+    private int myIndex = 0;
+    private Node myCurrent = myFirst;
+    private int myLocalIndex = 0;
+
+    public String toString(Node input){
+        StringBuilder string = new StringBuilder();
+
+        while(input != null){
+            string.append(input.info);
+            input = input.next;
+
+        }
+        return string.toString();
+
+
+    }
+
+
+
+    @Override
+    public long size() {
+        return mySize;
+
+    }
+
+    @Override
+    public void initialize(String source) {
+
+    }
+
+    @Override
+    public IDnaStrand getInstance(String source) {
+        return new LinkStrand(source);
+    }
+
+    @Override
+    public IDnaStrand append(String dna) {
+        myLast.next = new Node(dna);
+        myLast = myLast.next;
+        mySize = mySize + dna.length();
+        myAppends++;
+        return this;
+    }
+    public void frontAppend(String s){
+        Node n = new Node(s);
+        n.next = myFirst;
+        myFirst = n;
+        myAppends++;
+        mySize = mySize + myFirst.info.length();
+
+    }
+    @Override
+    public IDnaStrand reverse() {
+
+        LinkStrand str = new LinkStrand();
+        Node track = myFirst;
+        while(track != null){
+            StringBuilder copy = new StringBuilder(track.info);
+            copy.reverse();
+            this.frontAppend(copy.toString());
+            track = track.next;
+        }
+        return str;
+    }
+
+    @Override
+    public int getAppendCount() {
+        return myAppends;
+    }
+
+    @Override
+    public char charAt(int index) {
+
+        /*Node track = myFirst;
+        int count = 0;
+        int dex = 0;
+        while(track!= null){
+            count = count + track.info.length();
+            track = track.next;
+        }
+        track = myFirst;*/
+        if (index >= mySize || index < 0){
+            throw new IndexOutOfBoundsException();
+        }
+        if (myIndex > index){
+            myCurrent = myFirst;
+            myIndex = 0;
+            myLocalIndex = 0;
+
+        }
+        while(myIndex != index){
+            myIndex++;
+            myLocalIndex++;
+            if (myLocalIndex >= myCurrent.info.length()){
+                myCurrent = myCurrent.next;
+                myLocalIndex = 0;
+
+            }
+        }
+        return myCurrent.info.charAt(myLocalIndex);
+
+
+        /*
+        if(index > count){
+            throw new IndexOutOfBoundsException();
+        }
+        else{
+            if(index< myIndex){
+                count = 0;
+                while(count - 1< index) {
+                    count = count + track.info.length();
+                    track = track.next;
+                }
+            }
+            else{
+                count = myIndex;
+                track = myCurrent;
+
+                while(count - 1 < index){
+                    count = count + track.info.length();
+                    track = track.next;
+
+                }
+            }
+            myLocalIndex = myIndex - count;
+            myIndex = index;
+            myCurrent = track;
+
+            return track.info.charAt(myLocalIndex);
+*/
+
+
+
+    }
+}
